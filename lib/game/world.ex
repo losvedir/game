a2bab3852f694c6130a22fd3edddd93d703d72f3
Gabe Durazo <gabe@gabedurazo.com>
2017-11-27 21:53:50 -0500

    Generate and save coin locations on app start

---
+++ lib/game/world.ex
@@ -0,0 +1,31 @@
+defmodule Game.World do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    coins = Enum.map(0..99, fn _ -> random_pos() end)
+    {:ok, %{coins: coins}}
+  end
+
+  def coins do
+    GenServer.call(__MODULE__, :coins)
+  end
+
+  def handle_call(:coins, _from, %{coins: coins} = state) do
+    {:reply, coins, state}
+  end
+
+  defp random_pos do
+    %{
+      x: (:random.uniform(300) - 150),
+      y: (:random.uniform(300) - 150),
+    }
+  end
+end
+
+defmodule Game.World.State do
+  defstruct [coins: []]
+end
