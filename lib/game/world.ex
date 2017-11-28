f0eab2c0c597cfdab335c8b48af8c6c972e58ba3
Gabe Durazo <gabe@gabedurazo.com>
2017-11-28 00:21:48 -0500

    List of players in state

--- lib/game/world.ex
+++ lib/game/world.ex
@@ -1,3 +1,11 @@
+defmodule Game.World.State do
+  defstruct [coins: [], players: %{}]
+end
+
+defmodule Game.World.Player do
+  defstruct [position: %{x: 0, y: 0}]
+end
+
 defmodule Game.World do
   use GenServer
 
@@ -7,15 +15,36 @@ defmodule Game.World do
 
   def init(_) do
     coins = Enum.map(0..99, fn _ -> random_pos() end)
-    {:ok, %{coins: coins}}
+    {:ok, %Game.World.State{coins: coins}}
   end
 
   def coins do
     GenServer.call(__MODULE__, :coins)
   end
 
+  def new_player do
+    id = :rand.uniform() |> Float.to_string()
+    pos = random_pos()
+    player = %Game.World.Player{position: pos}
+
+    GenServer.call(__MODULE__, {:new_player, id, player})
+  end
+
+  def remove_player(id) do
+    GenServer.cast(__MODULE__, {:remove_player, id})
+  end
+
   def handle_call(:coins, _from, %{coins: coins} = state) do
     {:reply, coins, state}
+  end
+  def handle_call({:new_player, id, player}, _from, state) do
+    state = %{state | players: Map.put(state.players, id, player)}
+    {:reply, {:ok, id}, state}
+  end
+
+  def handle_cast({:remove_player, id}, state) do
+    state = %{state | players: Map.delete(state.players, id)}
+    {:noreply, state}
   end
 
   defp random_pos do
@@ -24,8 +53,4 @@ defmodule Game.World do
       y: (:rand.uniform(300) - 150),
     }
   end
-end
-
-defmodule Game.World.State do
-  defstruct [coins: []]
 end
--- lib/game_web/channels/world_channel.ex
+++ lib/game_web/channels/world_channel.ex
@@ -2,10 +2,17 @@ defmodule GameWeb.WorldChannel do
   use Phoenix.Channel
 
   def join("world", _message, socket) do
+    {:ok, player_id} = Game.World.new_player()
+    socket = assign(socket, :id, player_id)
     {:ok, socket}
   end
 
   def handle_in("coins", %{}, socket) do
     {:reply, {:ok, %{coins: Game.World.coins()}}, socket}
+  end
+
+  def terminate(_msg, socket) do
+    Game.World.remove_player(socket.assigns[:id])
+    :ok
   end
 end
